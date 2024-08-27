fd0b808693679d842c006a11400690b9816bfaae
Bae Ki Woong <12171790@inha.edu>
2024-08-27 23:14:22 +0900

    add: 배열 공부

--- ch01/hello.cpp
+++ ch01/hello.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-    for (const string& word : msg)
+    for (const string &word : msg)
     {
         cout << word << " ";
     }
     cout << endl;
 
     cout << size(msg) << endl;
+
 }
